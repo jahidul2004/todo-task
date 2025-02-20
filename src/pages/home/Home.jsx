243e85167881fb9f9f705111b624fe5008689b47
jahidul2004 <islamjahiduljihad@gmail.com>
2025-02-20 19:12:58 +0600

    Router setup completed.

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+
+const Home = () => {
+    return (
+        <div className="h-screen bg-gradient-to-br from-[#ff6867] flex flex-col items-center justify-center text-center">
+
+            <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">Welcome to Modern ToDo System
+            </h1>
+
+            <button className="btn mt-5 bg-gradient-to-tl from-[#ffffff] border-none shadow-none text-[#ff6867]">Get Started</button>
+        </div>
+    );
+};
+
+export default Home;

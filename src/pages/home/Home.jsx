bd0cd42db4be53745423d17f3d8efe6542301dd0
jahidul2004 <islamjahiduljihad@gmail.com>
2025-02-22 17:54:28 +0600

    Home page updated.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,11 +3,17 @@ import { Link } from "react-router-dom";
 const Home = () => {
     return (
         <div className="h-screen bg-gradient-to-br from-[#ff6867] flex flex-col items-center justify-center text-center">
-
-            <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">Welcome to Modern ToDo System
+            <h1 className="text-2xl md:text-3xl lg:text-6xl font-bold text-white">
+                Welcome to Modern <br />
+                ToDo System
             </h1>
 
-            <Link to={"/register"} className="btn mt-5 bg-gradient-to-tl from-[#ffffff] border-none shadow-none text-[#ff6867]">Get Started</Link>
+            <Link
+                to={"/register"}
+                className="btn mt-5 md:mt-8 bg-gradient-to-tl from-[#ffffff] border-none shadow-none text-[#ff6867]"
+            >
+                Get Started
+            </Link>
         </div>
     );
 };
